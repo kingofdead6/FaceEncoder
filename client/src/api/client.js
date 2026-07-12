c247593288b11a1ddbd9b07e6926b4a86c479bb0
kingofdead6 <ny_kahlouche@esi.dz>
2026-07-12 19:03:49 +0100

    Added new features

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "";
+const API_BASE = import.meta.env.VITE_API_URL || "https://faceencoder.onrender.com";
 
 export const http = axios.create({
   baseURL: API_BASE,
